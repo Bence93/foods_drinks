8d611272fce2bd08985e9ca33148a55fddef5e97
bence <b19930827@gmail.com>
2022-02-14 08:58:19 +0100

    Add project base

---
+++ src/main/java/com/example/foodsanddrinks/FoodsanddrinksApplication.java
@@ -0,0 +1,13 @@
+package com.example.foodsanddrinks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodsanddrinksApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoodsanddrinksApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/foodsanddrinks/FoodsanddrinksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.foodsanddrinks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodsanddrinksApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
