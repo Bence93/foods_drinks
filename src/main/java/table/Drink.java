bd91b629395bb993e342f0294a29e4fa41d458c2
arashd <b19930827@gmail.com>
2022-02-15 19:49:38 +0100

    Update gitignore + implement class properties

--- src/main/java/table/Drink.java
+++ src/main/java/table/Drink.java
@@ -1,4 +1,22 @@
 package table;
 
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Data
+@EqualsAndHashCode
+@NoArgsConstructor
 public class Drink {
+    private String name;
+    private double price;
+    private String group;
+
+    @Builder
+    public Drink(String name, double price, String group) {
+        this.name = name;
+        this.price = price;
+        this.group = group;
+    }
 }
--- src/main/java/table/Food.java
+++ src/main/java/table/Food.java
@@ -1,4 +1,22 @@
 package table;
 
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Data
+@EqualsAndHashCode
+@NoArgsConstructor
 public class Food {
+    private String name;
+    private double price;
+    private String group;
+
+    @Builder
+    public Food(String name, double price, String group) {
+        this.name = name;
+        this.price = price;
+        this.group = group;
+    }
 }
